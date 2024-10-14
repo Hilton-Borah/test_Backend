530a845f67967f8d7eafe79d4cfe0d0a37226544
Hilton-Borah <hiltonborah123@gmail.com>
2024-10-14 17:02:13 +0530

    deploy to ec2

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const Item = require('./model/Item');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const Item = require("./model/Item");
 
 const app = express();
 const PORT = 5000;
@@ -11,27 +11,34 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect('mongodb+srv://Hilton:hilton@cluster0.rgtirz5.mongodb.net/newTestingdeployment?retryWrites=true&w=majority', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log('MongoDB connected')).catch(err => console.log(err));
+// mongodb+srv://hiltonborah123:vccpSzsX2uYwQD9Q@testingcluster.hroxo.mongodb.net/?retryWrites=true&w=majority&appName=TestingCluster
+mongoose
+  .connect(
+    "mongodb+srv://hiltonborah123:vccpSzsX2uYwQD9Q@testingcluster.hroxo.mongodb.net/testdeployment?retryWrites=true&w=majority&appName=TestingCluster",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
 
 // Simple route
-app.get('/', (req, res) => {
-  res.send('Hello from the backend!');
+app.get("/", (req, res) => {
+  res.send("Hello from the backend!");
 });
 
-app.post('/items', async (req, res) => {
-    const newItem = new Item({ name: req.body.name });
-    await newItem.save();
-    res.status(201).json(newItem);
-  });
-  
-  // Get all items
-  app.get('/items', async (req, res) => {
-    const items = await Item.find();
-    res.json(items);
-  });
+app.post("/items", async (req, res) => {
+  const newItem = new Item({ name: req.body.name });
+  await newItem.save();
+  res.status(201).json(newItem);
+});
+
+// Get all items
+app.get("/items", async (req, res) => {
+  const items = await Item.find();
+  res.json(items);
+});
 
 // Start the server
 app.listen(PORT, () => {
